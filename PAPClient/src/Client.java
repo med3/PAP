7bdf223fa481f0684fa20feaa7cd28ba4d62bed5
Luis Brandelli <luisbrandelli@gmail.com>
2014-11-24 15:04:49 +0100

    Server allows multiple clients

--- PAPClient/src/Client.java
+++ PAPClient/src/Client.java
@@ -2,24 +2,21 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-
-	
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         String serverHostname = new String ("127.0.0.1");
 
         if (args.length > 0)
            serverHostname = args[0];
         System.out.println ("Attemping to connect to host " +
-		serverHostname + " on port 10007.");
+                serverHostname + " on port 10008.");
 
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
 
         try {
-            // echoSocket = new Socket("taranis", 7);
-            echoSocket = new Socket(serverHostname, 10007);
+            echoSocket = new Socket(serverHostname, 10008);
             out = new PrintWriter(echoSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
                                         echoSocket.getInputStream()));
@@ -36,12 +33,17 @@ public class Client {
                                    new InputStreamReader(System.in));
 	String userInput;
 
-        System.out.print ("input: ");
-	while ((userInput = stdIn.readLine()) != null) {
+        System.out.println ("Type Message (\"Bye.\" to quit)");
+	while ((userInput = stdIn.readLine()) != null) 
+           {
 	    out.println(userInput);
+
+            // end loop
+            if (userInput.equals("Bye."))
+                break;
+
 	    System.out.println("echo: " + in.readLine());
-            System.out.print ("input: ");
-	}
+	   }
 
 	out.close();
 	in.close();
@@ -49,5 +51,3 @@ public class Client {
 	echoSocket.close();
     }
 }
-
-
--- PAPServer/src/Server.java
+++ PAPServer/src/Server.java
@@ -1,54 +1,83 @@
 import java.net.*; 
 import java.io.*; 
 
-public class Server {
-	public static void main(String[] args) throws IOException 
-	   { 
-	    ServerSocket serverSocket = null; 
+public class Server extends Thread
+{ 
+ protected Socket clientSocket;
 
-	    try { 
-	         serverSocket = new ServerSocket(10007); 
-	        } 
-	    catch (IOException e) 
-	        { 
-	         System.err.println("Could not listen on port: 10007."); 
-	         System.exit(1); 
-	        } 
+ public static void main(String[] args) throws IOException 
+   { 
+    ServerSocket serverSocket = null; 
 
-	    Socket clientSocket = null; 
-	    System.out.println ("Waiting for connection.....");
+    try { 
+         serverSocket = new ServerSocket(10008); 
+         System.out.println ("Connection Socket Created");
+         try { 
+              while (true)
+                 {
+                  System.out.println ("Waiting for Connection");
+                  new Server (serverSocket.accept()); 
+                 }
+             } 
+         catch (IOException e) 
+             { 
+              System.err.println("Accept failed."); 
+              System.exit(1); 
+             } 
+        } 
+    catch (IOException e) 
+        { 
+         System.err.println("Could not listen on port: 10008."); 
+         System.exit(1); 
+        } 
+    finally
+        {
+         try {
+              serverSocket.close(); 
+             }
+         catch (IOException e)
+             { 
+              System.err.println("Could not close port: 10008."); 
+              System.exit(1); 
+             } 
+        }
+   }
 
-	    try { 
-	         clientSocket = serverSocket.accept(); 
-	        } 
-	    catch (IOException e) 
-	        { 
-	         System.err.println("Accept failed."); 
-	         System.exit(1); 
-	        } 
+ private Server (Socket clientSoc)
+   {
+    clientSocket = clientSoc;
+    start();
+   }
 
-	    System.out.println ("Connection successful");
-	    System.out.println ("Waiting for input.....");
+ public void run()
+   {
+    System.out.println ("New Communication Thread Started");
 
-	    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-	                                      true); 
-	    BufferedReader in = new BufferedReader( 
-	            new InputStreamReader( clientSocket.getInputStream())); 
+    try { 
+         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
+                                      true); 
+         BufferedReader in = new BufferedReader( 
+                 new InputStreamReader( clientSocket.getInputStream())); 
 
-	    String inputLine; 
+         String inputLine; 
 
-	    while ((inputLine = in.readLine()) != null) 
-	        { 
-	         System.out.println ("Server: " + inputLine); 
-	         out.println(inputLine); 
+         while ((inputLine = in.readLine()) != null) 
+             { 
+              System.out.println ("Server: " + inputLine); 
+              out.println(inputLine); 
 
-	         if (inputLine.equals("Bye.")) 
-	             break; 
-	        } 
+              if (inputLine.equals("Bye.")) 
+                  break; 
+             } 
 
-	    out.close(); 
-	    in.close(); 
-	    clientSocket.close(); 
-	    serverSocket.close(); 
-	   } 
-}
+         out.close(); 
+         in.close(); 
+         clientSocket.close(); 
+        } 
+    catch (IOException e) 
+        { 
+         System.err.println("Problem with Communication Server");
+         System.exit(1); 
+        } 
+    }
+} 
